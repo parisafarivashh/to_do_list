68568586f59ec3e3ca22e3b4c32a34d0b16b2d63
parisa <parisafarivash@gmail.com>
2022-11-16 15:13:44 +0330

    Added custom admin model

--- plan/to_do/admin.py
+++ plan/to_do/admin.py
@@ -1,7 +1,73 @@
 from django.contrib import admin
 
-from .models import Organization
+from .models import Organization, ToDo
 
 
-admin.site.register(Organization)
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+
+
+@admin.register(ToDo)
+class ToDoOAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'description',
+        'priority',
+        'tick',
+        'date',
+        'organization_id',
+        'organization_title',
+        'user_id',
+        'user_name',
+    ]
+    list_filter = [
+        'id',
+        'title',
+        'description',
+        'priority',
+        'tick',
+        'date',
+        'organization__id',
+        'organization__name',
+        'user_id',
+        'user__username',
+    ]
+    search_fields = [
+        'id',
+        'title',
+        'description',
+        'priority',
+        'tick',
+        'date',
+        'organization__id',
+        'organization__name',
+        'user__id',
+        'user__username',
+    ]
+    readonly_fields = [
+        'id',
+        'title',
+        'description',
+        'priority',
+        'tick',
+        'date',
+        'organization',
+        'user',
+    ]
+
+    def user_name(self, obj):
+        return obj.user.username
+
+    def user_id(self, obj):
+        return obj.user.id
+
+    def organization_id(self, obj):
+        return obj.organization.id
+
+    def organization_title(self, obj):
+        return obj.organization.name
 
--- plan/user/admin.py
+++ plan/user/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username']
+    list_filter = ['id', 'username']
+    search_fields = ['id', 'username']
+    readonly_fields = ['id', 'username']
+
